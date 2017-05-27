dae7a41a7322afd59b5ed6704a43d67de37c7d51
Robert Helms <robertjohnhelms@gmail.com>
2017-05-27 21:22:26 +0100

    Add basic routes and controllers and admin area.

---
+++ SimpleBlog_01/Controllers/PostsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog_01.Controllers
+{
+    public class PostsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("Hello, world!");
+        }
+    }
+}
