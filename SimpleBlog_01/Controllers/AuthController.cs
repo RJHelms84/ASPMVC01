10ae245c5fbcbd310e8a335ba550c82dcde9aab2
Robert Helms <robertjohnhelms@gmail.com>
2017-05-29 20:34:26 +0100

    View Models

--- SimpleBlog_01/Controllers/AuthController.cs
+++ SimpleBlog_01/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SimpleBlog_01.ViewModels;
 
 namespace SimpleBlog_01.Controllers
 {
@@ -10,7 +11,28 @@ namespace SimpleBlog_01.Controllers
     {
         public ActionResult Login()
         {
-            return View();
+            return View(new AuthLogin
+            {
+            });
+        }
+
+        //this attribute makes this method only take POST requests
+        //so GET requests are handled by method above
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(form);
+            }
+
+            if(form.Username != "Robert Helms")
+            {
+                ModelState.AddModelError("Username", "You are not Robert Helms!");
+                return View(form);
+            }
+
+            return Content("The form is valid!");
         }
     }
 }
